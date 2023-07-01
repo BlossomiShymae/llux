c4ce27ce72c28fd2412dafa994cdd4854897f0bd
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-07-01 02:06:03 -0500

    🚩 Add other HTTP methods and JSON flag

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Parser, ValueEnum};
-use irelia::{rest::LCUClient, LCUError, RequestClient};
-use serde_json::Value;
+use colored_json::to_colored_json_auto;
+use irelia::{rest::LCUClient, RequestClient};
+use miette::{miette, Result};
+use serde_json::{json, Value};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -10,6 +12,9 @@ struct Cli {
     request: RequestMethod,
     #[arg(help = "The LCU resource path e.g. '/lol-summoner/v1/current-summoner'")]
     path: String,
+    #[arg(long = "json")]
+    #[arg(help = "Send JSON data")]
+    json: Option<String>,
 }
 
 #[derive(ValueEnum, Debug, Clone)]
@@ -23,28 +28,68 @@ enum RequestMethod {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), LCUError> {
+async fn main() -> Result<()> {
     #[cfg(windows)]
     let _enabled = colored_json::enable_ansi_support();
 
+    // Start up the LCU client 🚀
     let args = Cli::parse();
-    let client = RequestClient::new();
-    let res = match LCUClient::new(&client) {
-        Ok(client) => {
-            let parts: Vec<&str> = args.path.as_str().split("//").collect();
-            let uri = parts.last().unwrap();
-            let path = format!("/{uri}");
-            match args.request {
-                RequestMethod::Get => client.get::<Value>(path.as_str()).await,
-                _ => panic!("Oh noes!"),
-            }
-        }
-        Err(e) => Err(e),
+    let r_client = RequestClient::new();
+    let client = LCUClient::new(&r_client);
+    let Ok(client) = client else {
+        let err = client.err().unwrap().to_string();
+        return Err((miette!("{}", err)).wrap_err("when connecting to LCU"));
     };
-    res.map(|v| {
-        v.map_or((), |v| {
-            println!("{}", colored_json::to_colored_json_auto(&v).unwrap())
-        })
-    })
-    .map_err(|e| e)
+
+    // Parse the LCU resource path ⚙
+    let parts: Vec<&str> = args.path.as_str().split("//").collect();
+    let Some(path) = parts.last() else {
+        let err = args.path.as_str();
+        return Err((miette!("{}", err)).wrap_err("when processing path"));
+    };
+    let path = format!("/{path}");
+
+    // Serialize body for potential use 🖨
+    let body = {
+        match args.json {
+            Some(json_string) => {
+                let value: Result<Value, serde_json::Error> =
+                    serde_json::from_str::<Value>(json_string.as_str());
+                match value {
+                    Ok(value) => Ok(Some(value)),
+                    Err(_) => Err((miette!("Bad JSON input")).wrap_err("when serializing body")),
+                }
+            }
+            None => Ok(None),
+        }
+    };
+    let Ok(body) = body else {
+        return Err(body.err().unwrap().into());
+    };
+
+    // Send request to the LCU 💜
+    let path = path.as_str();
+    let res = match args.request {
+        RequestMethod::Get => client.get::<Value>(path).await,
+        RequestMethod::Delete => client.delete::<Value>(path).await,
+        RequestMethod::Head => client.head::<Value>(path).await,
+        RequestMethod::Post => client.post::<Value, Value>(path, body).await,
+        RequestMethod::Put => client.put::<Value, Value>(path, body).await,
+        RequestMethod::Patch => client.patch::<Value, Value>(path, body).await,
+    };
+    let message = match res {
+        Ok(value) => {
+            let value = value.map_or(json!("undefined"), |v| v);
+            to_colored_json_auto(&value)
+                .map_err(|e| (miette!("{}", e.to_string())).wrap_err("when pretty printing JSON"))
+        }
+        Err(e) => Err(miette!("{}", e.to_string()).wrap_err("when processing LCU request")),
+    };
+
+    // End program 🌮
+    let Ok(m) = message else {
+        return Err(message.err().unwrap().into());
+    };
+    println!("{}", m);
+    Ok(())
 }

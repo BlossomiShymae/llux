f76c46935037aa0c333318393e7e6af20af68a81
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-07-05 03:44:05 -0500

    ✂ Refactor to use subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -1,64 +1,83 @@
-use std::ops::Deref;
-
-use clap::{Parser, ValueEnum};
+use clap::{Args, Parser, Subcommand, ValueEnum};
+use futures_util::StreamExt;
+use irelia::rest::LCUClient;
+use irelia::ws::{EventType, LCUWebSocket};
 use miette::miette;
+use miette::Result;
 use serde_json::Value;
 
-#[derive(Parser, Debug)]
+use crate::ws::{MinimalMessage, VerboseMessage, WebSocketMessage};
+
+#[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    #[arg(required_unless_present = "info")]
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    // Send a web request to the LCU
+    Request(RequestArgs),
+    // Subscribe and listen to a WebSocket event
+    Subscribe(SubscribeArgs),
+    // Display the current port and authorization
+    Info,
+}
+
+#[derive(Args)]
+pub struct RequestArgs {
+    #[arg(help = "The resource path e.g. //lol-summoner/v1/current-summoner")]
+    path: String,
     #[arg(
-        help = "The resource path or event to listen e.g. //lol-summoner/v1/current-summoner or wss://OnJsonApiEvent"
+        short = 'X',
+        long = "request",
+        value_enum,
+        default_value_t=RequestMethod::Get
     )]
-    pub path: Option<String>,
-    #[arg(long = "info")]
-    #[arg(help = "Display port and authorization")]
-    pub info: bool,
-    #[arg(short = 'X', long = "request", value_enum, default_value_t=RequestMethod::Get)]
-    #[arg(help = "Use request method (REST only)")]
-    pub request: RequestMethod,
-    #[arg(long = "json")]
-    #[arg(help = "Send JSON data (REST only)")]
-    pub json: Option<String>,
-    #[arg(short = 'v', long = "verbose")]
-    #[arg(help = "Print detailed data (WebSocket only)")]
-    pub verbose: bool,
-    #[arg(long = "filter")]
-    #[arg(help = "Filter event URI (case-sensitive) (WebSocket only)")]
-    pub filter: Option<String>,
+    request: RequestMethod,
+    #[arg(long = "json", help = "Send JSON data")]
+    json: Option<String>,
 }
 
-pub enum Protocol {
-    Rest(String),
-    WSS(String),
-}
+impl RequestArgs {
+    pub async fn execute(&self, client: &'_ LCUClient<'_>) -> Result<(), miette::ErrReport> {
+        let body = self.request_body()?;
+        let path = self.rest_path()?;
+        let res =
+            super::rest::send_request(&client, self.request.clone(), path.as_str(), body).await;
+        let message = match res {
+            Ok(value) => {
+                let value = value.map_or(serde_json::json!("undefined"), |v| v);
+                colored_json::to_colored_json_auto(&value).map_err(|e| {
+                    ewrap::<()>(&e.to_string(), "when pretty printing JSON").unwrap_err()
+                })
+            }
+            Err(e) => ewrap(&e.to_string(), "when processing LCU request"),
+        };
 
-impl Cli {
+        // End program 🌮
+        let m = message?;
+        println!("{}", m);
+        Ok(())
+    }
+
     fn path_parts(&self) -> Vec<String> {
-        let path = self.path.clone().unwrap();
-        path.as_str().split("//").map(str::to_string).collect()
+        let path = self.path.clone();
+        path.as_str()
+            .split("//")
+            .map(str::to_string)
+            .collect::<Vec<String>>()
     }
 
     fn parsed_path(&self) -> Result<String, miette::ErrReport> {
         match self.path_parts().last() {
             Some(path) => Ok(path.to_string()),
-            None => ewrap(self.path.clone().unwrap().as_str(), "when processing path"),
+            None => ewrap(self.path.clone().as_str(), "when processing path"),
         }
     }
 
-    pub fn protocol(&self) -> Result<Protocol, miette::ErrReport> {
-        let path = self.parsed_path()?;
-        match self.path_parts().first() {
-            Some(protocol) => match protocol.deref() {
-                "wss:" => Ok(Protocol::WSS(path)),
-                _ => Ok(Protocol::Rest(path)),
-            },
-            None => ewrap(&path, "when processing protocol"),
-        }
-    }
-
-    pub fn rest_path(&self) -> Result<String, miette::ErrReport> {
+    fn rest_path(&self) -> Result<String, miette::ErrReport> {
         let path = self.parsed_path()?;
         match path.chars().next() {
             Some(char) => {
@@ -72,7 +91,7 @@ impl Cli {
         }
     }
 
-    pub fn request_body(&self) -> Result<Option<Value>, miette::ErrReport> {
+    fn request_body(&self) -> Result<Option<Value>, miette::ErrReport> {
         match &self.json {
             Some(string) => {
                 let value = serde_json::from_str::<Value>(string.as_str());
@@ -82,6 +101,61 @@ impl Cli {
                 }
             }
             None => Ok(None),
+        }
+    }
+}
+
+#[derive(Args)]
+pub struct SubscribeArgs {
+    #[arg(help = "The WebSocket event e.g. OnJsonApiEvent")]
+    event: String,
+    #[arg(long = "filter", help = "Filter event URI (case-sensitive)")]
+    filter: Option<String>,
+    #[arg(short = 'v', long = "verbose", help = "Print detailed data")]
+    verbose: bool,
+}
+
+impl SubscribeArgs {
+    pub async fn execute(&self) -> Result<()> {
+        // Listen to WebSocket event
+        match LCUWebSocket::new().await {
+            Ok(mut ws) => {
+                let event = match self.event.as_str() {
+                    "OnJsonApiEvent" => Ok(EventType::OnJsonApiEvent),
+                    "OnLcdEvent" => Ok(EventType::OnLcdEvent),
+                    _ => ewrap(&self.event, "when processing websocket event"),
+                };
+                ws.subscribe(event?);
+                while let Some(event) = ws.next().await {
+                    let Ok(value) = event else {
+                        return ewrap(&event.err().unwrap().to_string(), "when processing websocket");
+                    };
+                    let wsm = WebSocketMessage::from(&value);
+                    // I don't know about this... :c
+                    match &self.verbose {
+                        true => match &self.filter {
+                            Some(name) => {
+                                if wsm.uri.contains(name) {
+                                    println!("{}", VerboseMessage::from(&wsm))
+                                }
+                            }
+                            None => println!("{}", VerboseMessage::from(&wsm)),
+                        },
+                        false => match &self.filter {
+                            Some(name) => {
+                                if wsm.uri.contains(name) {
+                                    println!("{}", MinimalMessage::from(&wsm));
+                                }
+                            }
+                            None => println!("{}", MinimalMessage::from(&wsm)),
+                        },
+                    };
+                }
+                Ok(())
+            }
+            Err(e) => {
+                return ewrap(&e.to_string(), "when creating websocket");
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 use clap::Parser;
-use colored_json::to_colored_json_auto;
-use futures_util::StreamExt;
-use irelia::{
-    rest::LCUClient,
-    ws::{EventType, LCUWebSocket},
-    RequestClient,
-};
+use irelia::{rest::LCUClient, RequestClient};
 use miette::Result;
-use serde_json::json;
 
-use cli::{ewrap, Cli};
+use cli::{ewrap, Cli, Commands};
 
 use crate::rest::ClientInfo;
-use crate::ws::{MinimalMessage, VerboseMessage, WebSocketMessage};
 
 pub mod cli;
 pub mod rest;
@@ -32,70 +24,14 @@ async fn main() -> Result<()> {
         return ewrap(&err, "when connecting to the LCU");
     };
 
-    // Display port and auth only
-    if cli.info {
-        println!("{}", ClientInfo::from(&client));
-        return Ok(());
-    }
-
-    let protocol = cli.protocol()?;
-    if let cli::Protocol::WSS(event) = protocol {
-        // Listen to WebSocket event
-        match LCUWebSocket::new().await {
-            Ok(mut ws) => {
-                let event = match event.as_str() {
-                    "OnJsonApiEvent" => Ok(EventType::OnJsonApiEvent),
-                    "OnLcdEvent" => Ok(EventType::OnLcdEvent),
-                    _ => ewrap(&event, "when processing websocket event"),
-                };
-                ws.subscribe(event?);
-                while let Some(event) = ws.next().await {
-                    let Ok(value) = event else {
-                        return ewrap(&event.err().unwrap().to_string(), "when processing websocket");
-                    };
-                    let wsm = WebSocketMessage::from(&value);
-                    // I don't know about this... :c
-                    match cli.verbose {
-                        true => match &cli.filter {
-                            Some(name) => {
-                                if wsm.uri.contains(name) {
-                                    println!("{}", VerboseMessage::from(&wsm))
-                                }
-                            }
-                            None => println!("{}", VerboseMessage::from(&wsm)),
-                        },
-                        false => match &cli.filter {
-                            Some(name) => {
-                                if wsm.uri.contains(name) {
-                                    println!("{}", MinimalMessage::from(&wsm));
-                                }
-                            }
-                            None => println!("{}", MinimalMessage::from(&wsm)),
-                        },
-                    };
-                }
-            }
-            Err(e) => {
-                return ewrap(&e.to_string(), "when creating websocket");
-            }
+    let status = match cli.command {
+        // Display port and authorization only
+        Commands::Info => {
+            println!("{}", ClientInfo::from(&client));
         }
+        // Subscribe to WebSocket event and listen for data
+        Commands::Subscribe(args) => args.execute().await?,
+        Commands::Request(args) => args.execute(&client).await?,
     };
-
-    // Send request to the LCU 💜
-    let body = cli.request_body()?;
-    let path = cli.rest_path()?;
-    let res = rest::send_request(&client, cli.request, path.as_str(), body).await;
-    let message = match res {
-        Ok(value) => {
-            let value = value.map_or(json!("undefined"), |v| v);
-            to_colored_json_auto(&value)
-                .map_err(|e| ewrap::<()>(&e.to_string(), "when pretty printing JSON").unwrap_err())
-        }
-        Err(e) => ewrap(&e.to_string(), "when processing LCU request"),
-    };
-
-    // End program 🌮
-    let m = message?;
-    println!("{}", m);
-    Ok(())
+    Ok(status)
 }
